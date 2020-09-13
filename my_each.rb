b5b5b49114d4c634786d53fba203971ae84c9164
MJ Johnson <mjohnson4685@gmail.com>
2020-09-13 21:39:15 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,26 @@
-def my_each # put argument(s) here
-  # code here
+list = ["ab", "cd", "ef"]
+
+def my_each(array)
+  counter = 0
+  while counter < array.length
+
+    yield(array[counter])
+    counter += 1
+  end
 end
+
+my_each(list) {|i| puts "#{i} are you going to drink that?"}
+
+
+def hello(array)
+  i = 0
+  basket = []
+  while i < array.length
+    basket << yield(array[i])
+    i += 1
+  end
+  basket
+end
+
+hello(list) {|i| "Yes #{i} go get your own!"}
+
