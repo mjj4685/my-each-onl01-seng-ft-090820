10ff140c9c41e21a06e1a2f47057d3089516d31b
MJ Johnson <mjohnson4685@gmail.com>
2020-09-13 22:05:40 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,25 +1,10 @@
-list = ["ab", "cd", "ef"]
-
-def my_each(array)
-  i = 0
-  while i < array.length
-# The "while" method will always return a value of => nil"
-    yield(array[i])
-    i = i + 1
-  end
-end
-
-my_each(list) {|i| "#{i} are you going to drink that?"}
-
-
-def hello(array)
-  i = 0
-  drinks = []
-  while i < array.length
-    drinks << yield(array[i])
-    i += 1
+def my_each(drinks)
+  counter = 0
+  while counter < drinks.length
+    yield(drinks[counter])
+    counter += 1
   end
   drinks
 end
 
-hello(list) {|i| "#{i} go get your own!"}
+    
